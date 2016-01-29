70ae5f16d3781e80f1462a02a182942405d41ae3
LexLoki <pietroribeiropepe@hotmail.com>
2016-01-29 18:27:40 -0200

    Initial setup

---
+++ game.lua
@@ -0,0 +1,25 @@
+game = {}
+
+function game.load()
+  
+end
+
+function game.start()
+  
+end
+
+function game.keypressed(dt)
+  
+end
+
+function game.update(dt)
+  
+end
+
+function game.draw()
+  local w = love.graphics.getWidth()
+  local h = love.graphics.getHeight()
+  love.graphics.setColor(255,0,0)
+  love.graphics.rectangle("fill", w/4, h/4, w/2, h/2)
+  love.graphics.setColor(255,255,255)
+end
---
+++ main.lua
@@ -0,0 +1,25 @@
+require "RPG_Full_Logo/RPG_Logo"
+require "game" 
+
+local current_step
+
+function love.load()
+  RPG_Logo.load(1.5,1.5,1.5,love.startGame)
+  current_step = RPG_Logo
+end
+
+function love.update(dt)
+  current_step.update(dt)
+end
+
+function love.draw()
+  current_step.draw()
+end
+
+function love.keypressed(key)
+  current_step.keypressed(key)
+end
+
+function love.startGame()
+  current_step = game
+end

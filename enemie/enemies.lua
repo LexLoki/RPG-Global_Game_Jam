36afc5e946b083cfe4cb05bac9faf434e6de3c6c
Augusto Baffa <abaffa@bol.com.br>
2016-01-31 03:02:10 -0200

    enemies adapter

---
+++ enemie/enemies.lua
@@ -0,0 +1,64 @@
+require "enemie/enemie_human"
+require "enemie/enemie_dog"
+
+enemies = {}
+enemies.list = {}
+
+function enemies.load()
+  
+  enemie_human.load()
+  enemie_dog.load()
+  
+  enemie_human.spawn(500, 200)
+  enemie_human.spawn(1000, 200)
+
+  enemie_dog.spawn(800, 400)
+  enemie_dog.spawn(400, 400)
+  
+  enemies.list = {enemie_human, enemie_dog}
+
+end
+
+function enemies.update(dt)
+   
+  for i_enemie, v_enemie in ipairs(enemies.list) do
+    v_enemie.update(dt)
+    for i, v in ipairs(v_enemie.list) do
+      mapManager.handleContact(dt,v)
+    end 
+  end
+end
+
+
+
+function enemies.draw()
+  for i_enemie, v_enemie in ipairs(enemies.list) do
+    v_enemie.draw()
+  end
+end
+
+
+
+function joinTables(t1, t2)
+ 
+   for k,v in ipairs(t2) do
+      table.insert(t1, v)
+   end 
+ 
+   return t1
+end
+
+function tableMerge(t1, t2)
+    for k,v in ipairs(t2) do
+    	if type(v) == "table" then
+    		if type(t1[k] or false) == "table" then
+    			tableMerge(t1[k] or {}, t2[k] or {})
+    		else  
+    			t1[k] = v
+    		end
+    	else
+    		t1[k] = v
+    	end
+    end
+    return t1
+end

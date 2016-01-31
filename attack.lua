5ebfd2fcc6b357d4891bb8b354c77374d12e25d0
Augusto Baffa <abaffa@bol.com.br>
2016-01-31 03:44:59 -0200

    correcao do damage e attacking

--- attack.lua
+++ attack.lua
@@ -6,31 +6,47 @@ function attack.load()
   attack.damage = false
   attack.ing = false
   timer_punch = 0
+  
+  attack.timer_damage = 0
+  damage_time = 0.5
 end
 function attack.start()
-  attack.x = 0
-  attack.y = 0
-  attack.width = 50
-  attack.height = 50
+  attack.x = player.x
+  attack.y = player.y
+  attack.width = player.width
+  attack.height = player.height
   attack.damage = false
   attack.ing = false
   timer_punch = 0
 end
 function attack.update(dt)
-  attack.x = player.x + player.width/2
-  attack.y = player.y + player.height/2
+  attack.x = player.x --+ player.width/2
+  attack.y = player.y --+ player.height/2
+  
+  -- soca em 3.5 segundos e desfaz após 0.5 segundos
   timer_punch = timer_punch + dt
-  if (timer_punch >= 3.5) then
-    attack.ing = true
+  if (timer_punch >= 4.0) then
+    attack.ing = false
     timer_punch = 0
+  elseif (timer_punch >= 3.5) then
+    attack.ing = true
   end
 
+  if attack.damage and attack.timer_damage > damage_time then
+      attack.damage = false
+      attack.timer_damage = 0
+  elseif attack.damage then
+       attack.timer_damage = attack.timer_damage + dt
+  end
 
   for i_enemie, v_enemie in ipairs(enemies.list) do
-    v_enemie.update(dt)
     for i, enemie in ipairs(v_enemie.list) do
-      if(CheckBoxCollision(attack.x,attack.y,attack.width,attack.height,enemie.x,enemie.y,64,96)) and attack.ing then
+      if(CheckBoxCollision(attack.x,attack.y,attack.width,attack.height,enemie.x,enemie.y,enemie.width, enemie.height))
+      --and attack.ing 
+      then
+
           attack.damage = true 
+          attack.timer_damage = 0
           table.remove(v_enemie.list, i)
       end
     end
@@ -38,8 +54,22 @@ function attack.update(dt)
 end
 function attack.draw()
   local c = mapManager.camera
+  
+  if attack.damage then
+    love.graphics.setColor(255,0,0)
+    love.graphics.rectangle("line", attack.x-c.pos_x, attack.y-c.pos_y, attack.width, attack.height)
+  end
+  
+   if attack.ing then
+    love.graphics.setColor(255,255,255)
+    love.graphics.rectangle("line", attack.x-c.pos_x, attack.y-c.pos_y, attack.width, attack.height)
+  end
+
+  --[[
   love.graphics.rectangle("line", attack.x-c.pos_x, attack.y-c.pos_y, attack.width, attack.height)
   love.graphics.print(tostring(attack.ing), 200, 300)
+  love.graphics.print(tostring(attack.damage), 300, 400)
+  ]]
 end
 function attack.keypressed(key)
   
@@ -49,5 +79,5 @@ function attack.keyreleased(key)
   
 end
 function CheckBoxCollision(x1,y1,w1,h1,x2,y2,w2,h2)
-  return x1 < x2+w2 and x2 < x1+w1 and y1 < y2+h2 and y2 < y1+h1
-end
+  return (x1 < x2+w2 and x2 < x1+w1 and y1 < y2+h2 and y2 < y1+h1)
+end 
